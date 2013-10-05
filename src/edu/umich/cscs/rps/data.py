b931bfa56b4c4458e04eb9eb0ef321c5325eff0c
Michael Bommarito <michael@resolveable.com>
2013-10-05 14:43:12 -0400

    Adding source directory with agents and data modules.

---
+++ src/edu/umich/cscs/rps/data.py
@@ -0,0 +1,17 @@
+'''
+Created on Oct 5, 2013
+
+@author: mjbommar
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(selfparams):
+        '''
+        Constructor
+        '''
+        
