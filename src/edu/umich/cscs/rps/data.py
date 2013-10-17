936c279764f1a228e55dd2bff0f890ee8298db6e
Michael Bommarito <michael.bommarito@solidlogic.com>
2013-10-17 15:50:55 -0400

    Refactoring GameRecord object into data

--- src/edu/umich/cscs/rps/data.py
+++ src/edu/umich/cscs/rps/data.py
@@ -4,14 +4,27 @@ Created on Oct 5, 2013
 @author: mjbommar
 '''
 
-class MyClass(object):
+
+class GameRecord(object):
     '''
-    classdocs
+    This is the record of a single engagement
+    To make lookup faster, store two records per engagement.
     '''
 
-
-    def __init__(selfparams):
+    def __init__(self, aNameId, aMove, aScore, bNameId, bMove, bScore):
         '''
         Constructor
         '''
-        
+        self.aNameId = aNameId  # player pointer
+        self.aMove = aMove
+        self.aScore = aScore
+        self.bNameId = bNameId
+        self.bMove = bMove
+        self.bScore = bScore
+
+    def __repr__(self):
+        '''
+        String representation of the object
+        '''
+        return "GameRecord(aNameId={0}, aMove={1}, aScore={2}, bNameId={3}, bMove={4}, outcome={4})".format(\
+                     self.aNameId, self.aMove, self.aScore, self.bNameId, self.bMove, self.bScore)
